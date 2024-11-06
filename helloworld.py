a55cbad54b55db4014fb8377aedd5caa6b348a50
Landeremmanson <solomonemmanson491@gmail.com>
2024-11-06 22:25:00 +0100

    first commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+"""
+Author: Brother Nelson
+
+Purpose: Display text to the screen.
+"""
+
+color = input('What is your favorite color? ')
+print('Your favorite color is')
+print(color)
+color = input('my favorite color is Yellow')
+print('your favorite color is Yellow')
